ac21db6c2805f157e3786ff61721b5cf941b9279
yy-cc-20 <chuyanyang01@gmail.com>
2025-01-15 00:26:06 +0800

    Fail test to test ci pipeline

--- src/main/java/com/mockcompany/webapp/controller/SearchController.java
+++ src/main/java/com/mockcompany/webapp/controller/SearchController.java
@@ -57,7 +57,7 @@ public class SearchController {
      */
     @GetMapping("/api/products/search")
     public Collection<ProductItem> search(@RequestParam("query") String query) {
-        return Collections.emptyList();
+        return this.productItemRepository.findAll();
         //return this.searchService.search(query);
     }
 }
